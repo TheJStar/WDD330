b9321cf9626c950dce7cb087e447a7404d94dde6
TheeJStar <kingoflightj@gmail.com>
2024-11-09 13:18:03 -0800

    fixed the cart - joel

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart")
+
+  if (!Array.isArray(cart)) {
+    cart = []
+  }
+
+
+  cart.push(product)
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
