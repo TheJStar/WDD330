0253a596ad6d9378b91aab6fa4aab0f63cf459ff
anxbell <valerieanabell@gmail.com>
2024-11-15 16:31:31 -0600

    Dynamic Product List added

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ProductData from "./ProductData.mjs";
+
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
+
+listing.init();
---
+++ src/js/productList.mjs
@@ -0,0 +1,38 @@
+import {renderListWithTemplate} from "./utils.mjs";
+
+class ProductListing {
+
+    constructor(category, dataSource, listElement) {
+
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        // our dataSource will return a Promise...so we can use await to resolve it.
+        const list = await this.dataSource.getData();
+        this.renderList("list")
+    }
+
+    renderList(list){
+
+        renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false)
+    }
+
+    
+}
+
+
+export default ProductListing;
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+        <a href="product_pages/index.html?product=">
+            <img src="" alt="Image of ">
+            <h3 class="card__brand"></h3>
+            <h2 class="card__name"></h2>
+            <p class="product-card__price">$</p>
+        </a>
+        </li>`
+}
