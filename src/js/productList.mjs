790dc5186af632cdb9646e67ebcf6e11227c82f9
anxbell <valerieanabell@gmail.com>
2024-11-15 17:54:55 -0600

    removed product-card class from html

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,6 +1,6 @@
-import {renderListWithTemplate} from "./utils.mjs";
+import renderListWithTemplate from "./utils.mjs";
 
-class ProductListing {
+export default class ProductListing {
 
     constructor(category, dataSource, listElement) {
 
@@ -10,29 +10,39 @@ class ProductListing {
     }
 
     async init() {
+
         // our dataSource will return a Promise...so we can use await to resolve it.
         const list = await this.dataSource.getData();
-        this.renderList("list")
+        //filtered by id to display only four
+        const filteredList = this.filterProducts(list, ["880RR", "985RF", "985PR", "344YJ"]);
+        this.renderList(filteredList)
+
+    }
+     //   render before doing the stretch
+    // renderList(filteredList) {
+    //     const htmlStrings = filteredList.map(productCardTemplate);
+    //     this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    // }
+
+    filterProducts(products, requiredIds) {
+        return products.filter(product => requiredIds.includes(product.Id));
     }
 
-    renderList(list){
-
-        renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false)
+    renderList(filteredList){
+        const parentElement = this.listElement;
+        renderListWithTemplate(productCardTemplate, parentElement, filteredList)
     }
 
     
 }
 
-
-export default ProductListing;
-
 function productCardTemplate(product) {
     return `<li class="product-card">
-        <a href="product_pages/index.html?product=">
-            <img src="" alt="Image of ">
-            <h3 class="card__brand"></h3>
-            <h2 class="card__name"></h2>
-            <p class="product-card__price">$</p>
+        <a href="product_pages/index.html?product=${product.Id}">
+            <img src="${product.Image}" alt="Image of ${product.Name}">
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Name}</h2>
+            <p class="product-card__price">$${product.FinalPrice}</p>
         </a>
         </li>`
 }
