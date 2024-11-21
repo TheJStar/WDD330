1b33d7319cd1b59f8b714bbf6fb5019e387f839f
anxbell <valerieanabell@gmail.com>
2024-11-21 09:45:01 -0600

    cart bubble

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,48 +1,40 @@
-import renderListWithTemplate from "./utils.mjs";
+// generates a list of product cards in HTML from an array
+import ProductData from "./ProductData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
 
+//return a template literal string for each of the templates needed
+function productCardTemplate(product) {
+    return `<li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
+            <img src=${product.Image}
+              alt=${product.Name} />
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Name}</h2>
+            <p class="product-card__price">$${product.ListPrice}</p>
+          </a>
+          </li>`
+}
 export default class ProductListing {
-
-    constructor(category, dataSource, listElement) {
-
+    constructor(category, dataSource, listElement ) {
         this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
 
     async init() {
-
-        // our dataSource will return a Promise...so we can use await to resolve it.
         const list = await this.dataSource.getData();
-        //filtered by id to display only four
-        const filteredList = this.filterProducts(list, ["880RR", "985RF", "985PR", "344YJ"]);
-        this.renderList(filteredList)
-
-    }
-     //   render before doing the stretch
-    // renderList(filteredList) {
-    //     const htmlStrings = filteredList.map(productCardTemplate);
-    //     this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-    // }
-
-    filterProducts(products, requiredIds) {
-        return products.filter(product => requiredIds.includes(product.Id));
+        this.renderList(list);
     }
 
-    renderList(filteredList){
-        const parentElement = this.listElement;
-        renderListWithTemplate(productCardTemplate, parentElement, filteredList)
+    renderList(list) {
+        list = this.filterList(list);
+        renderListWithTemplate(productCardTemplate, this.listElement, list)
     }
 
-    
-}
-
-function productCardTemplate(product) {
-    return `<li class="product-card">
-        <a href="product_pages/index.html?product=${product.Id}">
-            <img src="${product.Image}" alt="Image of ${product.Name}">
-            <h3 class="card__brand">${product.Brand.Name}</h3>
-            <h2 class="card__name">${product.Name}</h2>
-            <p class="product-card__price">$${product.FinalPrice}</p>
-        </a>
-        </li>`
+    // filter the list of products to just the four (4) needed ones
+    filterList(list) {
+        const productIdList = ["880RR", "985RF", "985PR", "344YJ"];
+        const filteredList = list.filter((element) => productIdList.includes(element.Id));
+        return filteredList;
+    }
 }
