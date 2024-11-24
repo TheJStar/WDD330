b7402f8c4fc62aa6ca25ebaf7ff5d7dbf722dce4
anxbell <valerieanabell@gmail.com>
2024-11-23 21:58:25 -0600

    misspelled filename

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,36 +1,35 @@
 import {
-    getLocalStorage,
-    setLocalStorage,
-    changeValueFromKeyList,
-  } from "./utils.mjs";
+  getLocalStorage,
+  setLocalStorage,
+  changeValueFromKeyList,
+} from "./utils.mjs";
 
 import { loadHeaderFooter } from "./utils.mjs";
 
-  
-  const cartIconNotification = document.querySelector(".item-count");
-  
-  function renderCartContents() {
-    const cartItems = getLocalStorage("so-cart");
-    if (!cartItems) {
-      const cartTotal = document.querySelector(".cart-footer");
-      cartTotal.setAttribute("class", "hide");
-    }
-  
-    if (!Array.isArray(cartItems) || cartItems.length === 0) {
-      document.querySelector(".product-list").innerHTML =
-        "<p>Your cart is empty</p>";
-      return;
-    }
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-    attachRemoveListeners();
-  
-    calculateTotal(cartItems);
+const cartIconNotification = document.querySelector(".item-count");
+
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  if (!cartItems) {
+    const cartTotal = document.querySelector(".cart-footer");
+    cartTotal.setAttribute("class", "hide");
   }
-  
-  function cartItemTemplate(item) {
-    const imageUrl = item.Images.PrimaryMedium 
-    const newItem = `<li class="cart-card divider">
+
+  if (!Array.isArray(cartItems) || cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML =
+      "<p>Your cart is empty</p>";
+    return;
+  }
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  attachRemoveListeners();
+
+  calculateTotal(cartItems);
+}
+
+function cartItemTemplate(item) {
+  const imageUrl = item.Images.PrimaryMedium;
+  const newItem = `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
         src="${imageUrl}"
@@ -46,45 +45,45 @@ import { loadHeaderFooter } from "./utils.mjs";
     <!-- Remove Button -->
       <button class="remove-item" data-id="${item.Id}">X</button>
   </li>`;
-  
-    return newItem;
-  }
-  
-  function attachRemoveListeners() {
-    // Attach event listeners to each remove button (X)
-    const removeButtons = document.querySelectorAll(".remove-item");
-  
-    removeButtons.forEach((button) => {
-      button.addEventListener("click", function () {
-        const itemId = button.getAttribute("data-id");
-        removeItemFromCart(itemId);
-      });
+
+  return newItem;
+}
+
+function attachRemoveListeners() {
+  // Attach event listeners to each remove button (X)
+  const removeButtons = document.querySelectorAll(".remove-item");
+
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const itemId = button.getAttribute("data-id");
+      removeItemFromCart(itemId);
     });
-  }
-  
-  function removeItemFromCart(itemId) {
-    // we need to fin the way to delite only one item !!!!
-    let cart = getLocalStorage("so-cart") || [];
-  
-    // Remove the item with the given ID
-    cart = cart.filter((item) => item.Id !== itemId);
-  
-    // Update localStorage with the new cart
-    setLocalStorage("so-cart", cart);
-  
-    // Re-render the cart after removing the item
-    renderCartContents();
-  }
-  
-  function calculateTotal(cartItems) {
-    // Need to find a way not to display total after the item has been removed
-    let total = 0;
-    cartItems.map((item) => (total += item.FinalPrice * item.Quantity));
-    let cartPElement = document.querySelector(".cart-total");
-    cartPElement.innerHTML = "";
-    cartPElement.insertAdjacentHTML("afterbegin", `Total: $${total}`);
-  }
-  
-  loadHeaderFooter();
+  });
+}
+
+function removeItemFromCart(itemId) {
+  // we need to fin the way to delite only one item !!!!
+  let cart = getLocalStorage("so-cart") || [];
+
+  // Remove the item with the given ID
+  cart = cart.filter((item) => item.Id !== itemId);
+
+  // Update localStorage with the new cart
+  setLocalStorage("so-cart", cart);
+
+  // Re-render the cart after removing the item
   renderCartContents();
-  //changeValueFromKeyList(cartIconNotification, "so-cart");
+}
+
+function calculateTotal(cartItems) {
+  // Need to find a way not to display total after the item has been removed
+  let total = 0;
+  cartItems.map((item) => (total += item.FinalPrice * item.Quantity));
+  let cartPElement = document.querySelector(".cart-total");
+  cartPElement.innerHTML = "";
+  cartPElement.insertAdjacentHTML("afterbegin", `Total: $${total}`);
+}
+
+loadHeaderFooter();
+renderCartContents();
+//changeValueFromKeyList(cartIconNotification, "so-cart");
